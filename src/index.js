d6b6d5a0db2f4e506d130020e36109edc0885bfe
Adriana Fernando <adriana.drih@gmail.com>
2020-04-08 15:31:15 -0300

    iniciando o arquivo js utilizando função para o botão de cifrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+document.getElementById("buttonCifra").addEventListener("click",cifrar);
+function cifrar (event) {
+    event.preventDefault();
+    let offset= document.getElementById("offset").value;
+    let textBox= document.getElementById("box").value;
+    console.log (offset);
+    console.log (textBox);
+    document.getElementById ("box2").innerHTML=textBox;
+
+}
