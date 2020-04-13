b70ce9e8648415734bf16c1986490d3c29f2db02
Adriana Fernando <adriana.drih@gmail.com>
2020-04-13 08:01:32 -0300

    Codigo finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -4,9 +4,9 @@ const cipher = {
       throw new TypeError('Digite uma senha válida ou confirme se sua mensagem está correta.')
     }
     let resultado = "";
-    for (let i = 0; i< textBox.length; i++) {
-      let n = textBox.charCodeAt(i);
-      let codigo = (n - 65);
+    for (let letra = 0; letra< textBox.length; letra++) {
+      let num = textBox.charCodeAt(letra); //funcao que transforma letra em numero da tabela asc
+      let codigo = (num - 65);
       let deslocamento = (((codigo + offset)%26)+ 65); 
       resultado += String.fromCharCode(deslocamento);
 
@@ -17,9 +17,9 @@ const cipher = {
   },
   decode: function (offset, textBox) {
   let resultado = "";
-  for (let i = 0; i< textBox.length; i++) {
-    let n = textBox.charCodeAt(i);
-    let codigo = (n - 90);
+  for (let letra = 0; letra< textBox.length; letra++) {
+    let num = textBox.charCodeAt(letra);
+    let codigo = (num - 90);
     let deslocamento = (((codigo - offset)%26)+ 90); 
     resultado += String.fromCharCode(deslocamento);
 
